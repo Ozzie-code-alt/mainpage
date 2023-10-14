c2b9d502f979dbd5d064278f958206d8266df258
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-10-14 21:23:44 +0800

    SomecChanges

---
+++ src/app/components/MainPage.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"; // handle images
+
+
+
+const MainPage = () => {
+  return (
+    <>
+      <div className=" h-4/5 grid grid-cols-3">
+        <div className="left text-center grid gap-20">
+          <div className="text-8xl font-bold text-blue-400 grid gap-4">
+            Web <br/>
+            Design
+            <span className="text-5xl text-green-500"> Landing Page</span>
+          </div>
+
+          <div className="">
+            Ex proident ex sunt duis. Duis nulla ullamco occaecat dolor in
+            voluptate ullamco tempor. Amet nulla ut sint sint velit non in
+            pariatur consequat ullamco. Incididunt culpa amet pariatur e a
+
+          </div>
+
+          <div className="bg-green-900 h-10 w-28 justify-self-center justify-center flex items-center rounded-lg text-white"> <button> Learn More</button></div>
+        </div>
+
+        <div className="right col-span-2 flex justify-center">
+            <div >
+                <Image
+                    src={"discusison.svg"}
+                    alt="Discussion Image"
+                    width={720}
+                    height={500}
+                />
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar"
+import MainPage from "./components/MainPage"
+
+
+
 
 export default function Home() {
   return (
   <>
   <div className="">
     <Navbar/>
+    <MainPage/>
   </div>
 
   </>
